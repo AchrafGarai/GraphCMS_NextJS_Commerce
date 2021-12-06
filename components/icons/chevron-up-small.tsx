826ce09475c54d71fd7613a8e72b32d0684df066
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2021-12-06 10:16:00 +0100

    Changed Cart icon

--- components/icons/chevron-up-small.tsx
+++ components/icons/chevron-up-small.tsx
@@ -1,4 +1,4 @@
-function ChevronUpSmall(props) {
+const ChevronUpSmall = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- components/icons/github.tsx
+++ components/icons/github.tsx
@@ -1,4 +1,4 @@
-function GitHub(props) {
+const GitHub = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
