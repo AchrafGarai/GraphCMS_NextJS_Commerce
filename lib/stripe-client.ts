491e0a793e246bc86785631a57cde749a3c653e7
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2022-01-11 10:47:49 +0100

    Update stripe-client.ts

--- lib/stripe-client.ts
+++ lib/stripe-client.ts
@@ -1,3 +1,6 @@
 import Stripe from 'stripe'
 
-export default new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export default new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion:'2020-08-27'
+})
